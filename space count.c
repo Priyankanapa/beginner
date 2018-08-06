75f0220c8c9d5e5c9f278076cdd5642878d39d8d
Priyankanapa <36722932+Priyankanapa@users.noreply.github.com>
2018-08-06 16:10:49 +0530

    Create space count.c

---
+++ space count.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char str[50];
+int i,count=0;
+printf("\nEnter any string : ");
+gets(str);    
+for(i=0;str[i];i++)
+if(str[i] == ' ')
+count++;
+printf("%d",count);
+getch();
+}
